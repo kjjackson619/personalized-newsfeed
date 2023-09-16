c9f84f1e40850f7e422d7b813068efd9c338f2bb
KJ Jackson <kjjackson619@yahoo.com>
2023-09-15 21:04:19 -0400

    backend index setup

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({limit: '50mb'}));
+
+const startServer = async () => {
+    try {
+        app.listen(8080, () => console.log('Server has started on port http://localhost:8080'));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+startServer();

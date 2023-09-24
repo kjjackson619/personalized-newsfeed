a2154bf7577ecadd5ecc57cdf37054f8449f19ea
KJ Jackson <kjjackson619@yahoo.com>
2023-09-24 16:36:51 -0400

    implement JWT token and localStorage to Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -12,21 +12,35 @@ const Login = () => {
         setPassword(event.target.value);
     };
 
+    const loginUser = async(username, password) => {
+        const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({username, password})
+        };
+        try {
+            const response = await fetch('api/login', requestOptions);
+            if(!response.ok) {
+                throw new Error('Network response was not ok.');
+            }
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            throw new Error('Login failed');
+        }
+    }
+
     const handleSubmit = async(event) => {
         event.preventDefault();
-
         try {
             const response = await loginUser(username, password);
 
-            const token = response.token;
-
-            localStorage.setItem('token', token);
-
+            localStorage.setItem('token', response.token);
             console.log('Login successful! Token stored in localStorage.');
         } catch (error) {
-            console.log('Login failed: ', error.message);
+            console.error('Login failed', error.message);
         }
-    }
+    };
 
   return (
    <React.Fragment>

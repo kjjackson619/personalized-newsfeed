5dc3db7ab2c4054ca3dc40c8b4d46105ae41d1ab
KJ Jackson <kjjackson619@yahoo.com>
2023-09-24 16:49:02 -0400

    debugging

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import * as React from 'react';
+import Login from './components/Login';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <Login /> 
+    <React.Fragment>
+        <Login /> 
+    </React.Fragment>
+    
   )
 }
 
